066034a5c5f4191bdeb813f4f1bb80c85a4c9502
Dan Dart <git@dandart.co.uk>
2022-10-08 19:59:58 +0100

    update to ghc942

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 {
   nixpkgs ? import <nixpkgs> {},
-  compiler ? "ghc924",
+  compiler ? "ghc942",
   haskell-tools ? import (builtins.fetchTarball "https://github.com/danwdart/haskell-tools/archive/master.tar.gz") {
     nixpkgs = nixpkgs;
     compiler = compiler;
