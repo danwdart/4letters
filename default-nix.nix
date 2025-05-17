b064c256f4cfa56afa8da627ce9428cd19089377
Dan Dart <git@dandart.co.uk>
2025-05-17 19:56:48 +0100

    fix build

--- default-nix.nix
+++ default-nix.nix
@@ -1,6 +1,6 @@
 {
   nixpkgs ? import <nixpkgs> {},
-  compiler ? "ghc910",
+  compiler ? "ghc912",
   haskell-tools ? import (builtins.fetchTarball "https://github.com/danwdart/haskell-tools/archive/master.tar.gz") {
     nixpkgs = nixpkgs;
     compiler = compiler;
